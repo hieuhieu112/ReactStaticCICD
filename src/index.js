740ca189c51f4e7106c4208eac97195d8c445c3e
crush24h <87923370+PKTinh107@users.noreply.github.com>
2024-06-19 16:32:56 +0700

    hehe

--- src/index.js
+++ src/index.js
@@ -8,25 +8,13 @@ import store from './redux/store';
 
 
 import { Home, Product, Products, AboutPage, ContactPage, Cart, Login, Register, Checkout, PageNotFound } from "./pages"
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <Provider store={store}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product" element={<Products />} />
-        <Route path="/product/:id" element={<Product />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="*" element={<PageNotFound />} />
-        <Route path="/product/*" element={<PageNotFound />} />
-      </Routes>
+      <App />
     </Provider>
-  </BrowserRouter>
+  </React.StrictMode>
 );
-export default root;
