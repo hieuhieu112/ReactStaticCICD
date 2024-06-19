a79773e66aeddfc9ea326cea59b8ac2fb8a3ead6
crush24h <87923370+PKTinh107@users.noreply.github.com>
2024-06-19 17:06:31 +0700

    kk

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import store from './redux/store';
 import App from "./App";
 
-test("renders components correctly", async () => {
+test("renders comps", async () => {
   await act(async () => {
     <Provider store={store}>
       <App />
